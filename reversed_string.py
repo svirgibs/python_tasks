13fc51d1418b370428f8c246e5cb011170449f20
sg <svirgibs@gmail.com>
2022-07-29 15:43:11 +0300

    Задача 1: развернуть строку

--- reversed_string.py
+++ reversed_string.py
@@ -11,3 +11,11 @@ print("Первый способ [::-1]: %s" % reversed_string_1)
 # Второй способ
 reversed_string_2 = ''.join(reversed(default_string))
 print("Второй способ ''.join(reversed()): %s" % reversed_string_2)
+
+
+# Функция разворачивающая любую строку
+def reverse_string(string):
+    return string[::-1]
+
+
+print("Функция reverse_string(): %s" % reverse_string('Python'))
