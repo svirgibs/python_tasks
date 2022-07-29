62b723cc67dadb0382c22eb383ba98fb24354353
sg <svirgibs@gmail.com>
2022-07-29 15:37:58 +0300

    Задача 1: развернуть строку

---
+++ reversed_string.py
@@ -0,0 +1,13 @@
+"""
+    Дана строка. Нужно развернуть ее в обратную сторону 2мя способами
+"""
+
+default_string = "Hello, world!"
+
+# Первый способ
+reversed_string_1 = default_string[::-1]
+print("Первый способ [::-1]: %s" % reversed_string_1)
+
+# Второй способ
+reversed_string_2 = ''.join(reversed(default_string))
+print("Второй способ ''.join(reversed()): %s" % reversed_string_2)
